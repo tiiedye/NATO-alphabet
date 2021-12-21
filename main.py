8cc813215e7751f93801dc8f0be2d0463e3d26f0
tiiedye <tiiedye@gmail.com>
2021-12-21 13:28:19 -0800

    getting user input and printing a list of the nato codes

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pandas
 
 # TODO 1. Create a dictionary in this format:
 alphabet_df = pandas.read_csv("nato_phonetic_alphabet.csv")
-nato_dict = {letter: code for (letter, code) in alphabet_df.iterrows()}
+nato_dict = {row.letter: row.code for (index, row) in alphabet_df.iterrows()}
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+user_input = input("Enter a word: ").upper()
+code_list = [nato_dict[letter] for letter in user_input]
+print(code_list)
